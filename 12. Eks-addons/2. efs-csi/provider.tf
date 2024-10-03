561da23457744c7e9c2d21c00757cb4ce83deb9d
WemaDevOps <152240864+Wemadevops@users.noreply.github.com>
2024-10-02 21:36:30 -0400

    Update provider.tf

--- 12. Eks-addons/2. efs-csi/provider.tf
+++ 12. Eks-addons/2. efs-csi/provider.tf
@@ -13,12 +13,12 @@ terraform {
 
 # Provider block
 provider "aws" {
-  region = "us-east-1"
+  region = ""
 }
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  token                  = data.aws_eks_cluster_auth.token
 }
 
