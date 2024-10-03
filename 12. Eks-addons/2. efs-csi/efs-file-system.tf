60de1128d3ed37fa52380aaba84263b405a708d0
WemaDevOps <152240864+Wemadevops@users.noreply.github.com>
2024-10-02 21:35:47 -0400

    Update efs-file-system.tf

--- 12. Eks-addons/2. efs-csi/efs-file-system.tf
+++ 12. Eks-addons/2. efs-csi/efs-file-system.tf
@@ -1,5 +1,5 @@
 # EFS file system
-resource "aws_efs_file_system" "stw_node_efs" {
+resource "aws_efs_file_syste" "stw_node_efs" {
   creation_token = "efs-for-stw-node"
 
   tags = {
@@ -10,13 +10,13 @@ resource "aws_efs_file_system" "stw_node_efs" {
 #efs mount targets
 resource "aws_efs_mount_target" "stw_node_efs_mt_0" {
   file_system_id  = aws_efs_file_system.stw_node_efs.id
-  subnet_id = data.aws_subnets.nodes.ids[0]
-  security_groups = [data.aws_security_groups.efs_sg.ids[0]]
+  subnet_id = data.aws_subnets.nodes.ids
+  security_groups = [data.aws_security_groups.efs_sg.ids[0]
 }
 
 resource "aws_efs_mount_target" "stw_node_efs_mt_1" {
   file_system_id  = aws_efs_file_system.stw_node_efs.id
-  subnet_id = data.aws_subnets.nodes.ids[1]
+  subnet_id = data.aws_subnets.node.ids[1]
   security_groups = [data.aws_security_groups.efs_sg.ids[0]]
 }
 
